903ae75e1650c4d7c2945b9e4c862e2eb2df803e
Gary Hui <44025886+huiwy@users.noreply.github.com>
2020-03-09 20:06:23 +0800

    Lab 1

---
+++ ex5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
+
+#define check_size( t ) (printf( "Size of %s :  %ld \n", #t  , ( long int ) sizeof( t )))
+
+int main(){
+    check_size(char);
+    check_size(short);
+    check_size(short int);
+    check_size(int);
+    check_size(long int);
+    check_size(unsigned int);
+    check_size(void *);
+    check_size(size_t);
+    check_size(float);
+    check_size(double);
+    check_size(int8_t);
+    check_size(int16_t);
+    check_size(int32_t);
+    check_size(int64_t);
+    check_size(time_t);
+    check_size(clock_t);
+    check_size(struct tm);
+    check_size(NULL);
+
+    return 1;
+}
