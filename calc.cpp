cdf97f93b73302e8e2f66d6297c38a933bd4fca3
huiwy <3073665460@qq.com>
2020-05-22 05:45:28 +0800

    change the order of summation, and for some unknown reasons it speeds up

--- calc.cpp
+++ calc.cpp
@@ -2,7 +2,7 @@ void
 calc (int n, const int *a, const int *b, int *c)
 {
   #pragma omp parallel for
-  for (int k = 0; k < n; k++)
+  for (int k = n - 1; k >= 0; k--)
   {
     for (int i = 0; i < n - k; ++i)
     {
