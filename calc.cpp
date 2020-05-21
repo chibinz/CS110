3f3c4903cc055e61f76b145d3df0e68061f1402e
huiwy <3073665460@qq.com>
2020-05-22 06:13:13 +0800

    autolab

--- calc.cpp
+++ calc.cpp
@@ -1,10 +1,12 @@
+#include <emmintrin.h>
+
 void
 calc (int n, const int *a, const int *b, int *c)
 {
   #pragma omp parallel for
   for (int k = n - 1; k >= 0; k--)
   {
-    for (int i = 0; i < n - k; ++i)
+    for (int i = 0; i < (n - k) / 4 * 4; i += 4)
     {
       c[k] += a[i + k] * b[i];
     }
