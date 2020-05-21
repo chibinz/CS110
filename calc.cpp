9cb14afc8140fe99f3af549e243aa7a1d14bef65
huiwy <3073665460@qq.com>
2020-05-22 05:39:27 +0800

    add #pragma omp parallel for

--- calc.cpp
+++ calc.cpp
@@ -1,8 +1,13 @@
 void
 calc (int n, const int *a, const int *b, int *c)
 {
+  #pragma omp parallel for
   for (int k = 0; k < n; k++)
+  {
     for (int i = 0; i < n - k; ++i)
+    {
       c[k] += a[i + k] * b[i];
+    }
+  }
 }
 
