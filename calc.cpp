d1e604231ff2ce674b3a12d9916ab70b5d2a6074
huiwy <3073665460@qq.com>
2020-05-22 22:20:47 +0800

    original version

--- calc.cpp
+++ calc.cpp
@@ -3,8 +3,8 @@
 void
 calc (int n, const int *a, const int *b, int *c)
 {
-  //#pragma omp parallel for
-  for (int k = 0; k < n; k--)
+  #pragma omp parallel for
+  for (int k = 0; k < n; k++)
   {
     for (int i = 0; i < (n - k) / 4 * 4; i += 4)
     {
